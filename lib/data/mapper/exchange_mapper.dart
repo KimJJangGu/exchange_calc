a38154a35900588665836736c064823f97f4054e
Kim DoHyeon <dohyun51731@gmail.com>
2024-01-09 20:33:24 +0900

    refactor: mapper 내용 수정

--- lib/data/mapper/exchange_mapper.dart
+++ lib/data/mapper/exchange_mapper.dart
@@ -4,17 +4,18 @@ import 'package:exchange_calc/domain/model/exchange_model.dart';
 extension ExchangeMapper on ExchangeDto {
   ExchangeModel toExchangeModel() {
     return ExchangeModel(
-      exchangeRates: rates != null ? _toDouble(rates!) : <String, num>{},
+      exchangeRates: _toConcreteData(rates),
       lastUpdateTime: DateTime.fromMillisecondsSinceEpoch((timeLastUpdateUnix?.toInt() ?? 0) * 1000),
       nextUpdateTime: DateTime.fromMillisecondsSinceEpoch((timeNextUpdateUnix?.toInt() ?? 0) * 1000),
     );
   }
 
-  Map<String, num> _toDouble(Map<String, dynamic> data) {
-    Map<String, num> newMap = {};
-    for (var element in data.entries) {
-      newMap.putIfAbsent(element.key, () => element.value as num);
+  Map<String, num> _toConcreteData(Map<String, dynamic>? data) {
+    if (data == null) {
+      return {};
     }
-    return newMap;
+
+    return Map<String, num>.fromEntries(
+        data.entries.map((entry) => MapEntry(entry.key, entry.value as num)));
   }
 }
